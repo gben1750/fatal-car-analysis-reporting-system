14a3edab8a9fa96a9f5b0fa68b7e7fe5f2884333
gben1750 <gbenson21a@gmail.com>
2021-03-20 16:55:21 +1100

    Updating file names - incorrect file names originally defined.

--- tests/testthat/testfcars.R
+++ tests/testthat/testfcars.R
@@ -4,13 +4,13 @@ library(fatalcaranalysiisreports)
 context("Correct Number of records exist in the 2013 file")
 
 test_that("Test number of records in 2013 dataset should equal 30202", {
-  fileName = 'accident_2013.csv'
+  fileName = 'accident_2013.csv.bz2'
   tbl <- fars_read(fileName)
   expect_equal(nrow(tbl), 30202)
 })
 
-test_that("Test number of records in 2015 dataset should equal 30056", {
-  fileName = 'accident_2014.csv'
+test_that("Test number of records in 2014 dataset should equal 30056", {
+  fileName = 'accident_2014.csv.bz2'
   tbl <- fars_read(fileName)
   expect_equal(nrow(tbl), 30056)
 })
