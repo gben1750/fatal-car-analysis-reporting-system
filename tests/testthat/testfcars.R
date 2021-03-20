85a6f23b21dfce7722e6f2fe681ce611f0939658
gben1750 <gbenson21a@gmail.com>
2021-03-20 16:07:32 +1100

    Commit change to test script.

--- tests/testthat/testfcars.R
+++ tests/testthat/testfcars.R
@@ -9,7 +9,7 @@ test_that("Test number of records in 2013 dataset should equal 30202", {
   expect_equal(nrow(tbl), 30202)
 })
 
-test_that("Test number of records in 2014 dataset should equal 30056", {
+test_that("Test number of records in 2015 dataset should equal 30056", {
   fileName = 'accident_2014.csv'
   tbl <- fars_read(fileName)
   expect_equal(nrow(tbl), 30056)
